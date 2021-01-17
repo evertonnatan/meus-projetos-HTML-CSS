e827910c33700fd4dd4c2b2d8bf9e0cbd2940491
Everton Natã <37844708+evertonnatan@users.noreply.github.com>
2021-01-17 09:55:05 -0300

    Transferindo a página do portifólio para outro diretório

--- responsive_personal_portifolio/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menuToggle = document.querySelector('.toggle')
-    const sidebar = document.querySelector('.sidebar')
-    menuToggle.classList.toggle('active')
-    sidebar.classList.toggle('active')
-}
